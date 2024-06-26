e1b50f4875da96ef249baeef3f0a3a112f2a11f7
muskankousar147 <muskankousar@gmail.com>
2024-06-26 19:40:15 +0530

    sql task

---
+++ project.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE project;
+CREATE TABLE Airport_details(id int,airport_name varchar(20), airport_arrival int, airport_departure int, airport_price int);
+USE project;
+
+CREATE TABLE cricket_team(id int, team_name varchar(20), no_of_players int, captain varchar(30), coach varchar(20));
+USE project;
